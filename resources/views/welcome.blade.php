cad39a309bd5c31722901616dfaaa80e9405fb07
Abdelrhman Mgahed <abdelrhmanmgahed131@gmail.com>
2022-10-06 22:37:37 +0200

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Course Scraper</title>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
